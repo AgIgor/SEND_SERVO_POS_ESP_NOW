587be5ad4d0cc03e79a4e40745e9c3fb1b6e5237
Igor Angeli <agigor@outlook.com>
2023-04-30 17:15:33 -0300

    buzzer and setup init

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,18 @@
 #define LED_PIN 2
 #define SPEAKER 4
 
+#define DEBUG
+//#define DEBUG_DATA_SENT
+//#define DEBUG_PKG
+#define DEBUG_INPUT
+
 #define READ_PIN_A 18
 #define PULSE_PIN_A 19
 
-#define READ_PIN_B 23
-#define PULSE_PIN_B 22
+#define READ_PIN_B 22
+#define PULSE_PIN_B 23
 
-int freq = 4050, t = 126, dly = 230;
+int freq = 4050, t = 126, dly = 150;
 
 const uint32_t  ANALOG_TIMEOUT  = 500;  // = 20ms 20000
 
@@ -28,7 +33,7 @@ typedef struct struct_message {
 struct_message myData;
 
 void bipBuzzer(){
-  for(int i=0;i<2;i++){
+  for(int i=0;i<4;i++){
     tone(SPEAKER, freq, t);
     delay(dly);
     noTone(SPEAKER);
@@ -64,37 +69,26 @@ uint8_t analogDigitalRead(const uint8_t Ppin, const uint8_t Rpin) {
 }//end analogDigitalRead
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t sendStatus) { 
-  Serial.println(sendStatus == 0 ? "Entregue":"Não entregue");
+  #ifdef DEBUG_DATA_SENT
+    Serial.println(sendStatus == 0 ? "Entregue":"Não entregue");
+  #endif
   digitalWrite(LED_PIN, !sendStatus);
 
   if(sendStatus == 1)bipBuzzer();
 
 }//end OnDataSent
 
-void setup() {
-  pinMode(SPEAKER, OUTPUT);
-  for(int i=0;i<4;i++){
-    tone(SPEAKER, freq, t);
-    delay(dly);
-    noTone(SPEAKER);
-  }
-
-  Serial.begin(115200);
-  pinMode(LED_PIN,OUTPUT);
-  digitalWrite(LED_PIN,LOW);
-
-  pinMode(READ_PIN_A,   INPUT);
-  pinMode(PULSE_PIN_A,  OUTPUT);
-
-  pinMode(READ_PIN_B,   INPUT);
-  pinMode(PULSE_PIN_B,  OUTPUT);
-
+void wifi_init(){
   WiFi.mode(WIFI_STA);
   WiFi.setTxPower(WIFI_POWER_19_5dBm);
-  Serial.print("TX WiFi.macAddress: ");
-  Serial.print(WiFi.macAddress());
-  Serial.print(" Power: ");
-  Serial.println(WiFi.getTxPower());
+
+  #ifdef DEBUG 
+    Serial.print("TX WiFi.macAddress: ");
+    Serial.print(WiFi.macAddress());
+    Serial.print(" Power: ");
+    Serial.println(WiFi.getTxPower());
+  #endif
+
   /*
       Available ESP32 RF power parameters:
       WIFI_POWER_19_5dBm    // 19.5dBm (For 19.5dBm of output, highest. Supply current ~150mA)
@@ -112,9 +106,14 @@ void setup() {
       Available ESP8266 RF power parameters:
       0    (for lowest RF power output, supply current ~ 70mA
       20.5 (for highest RF power output, supply current ~ 80mA
-  */ 
+  */
+}//end wifi init
+
+void esp_now_start(){
   if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
+    #ifdef DEBUG 
+      Serial.println("Error initializing ESP-NOW");
+    #endif
     return;
   }
   esp_now_peer_info_t peerInfo;
@@ -126,10 +125,52 @@ void setup() {
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("Failed to add peer");
+    #ifdef DEBUG 
+      Serial.println("Failed to add peer");
+    #endif
     return;
+  }  
+}//end esp now start
+
+void setup() {
+  #ifdef DEBUG 
+    Serial.begin(115200);
+  #endif
+
+  pinMode(SPEAKER, OUTPUT);
+  pinMode(LED_PIN,OUTPUT);
+  digitalWrite(LED_PIN,LOW);
+
+  pinMode(READ_PIN_A,   INPUT);
+  pinMode(PULSE_PIN_A,  OUTPUT);
+
+  pinMode(READ_PIN_B,   INPUT);
+  pinMode(PULSE_PIN_B,  OUTPUT);
+
+  uint8_t adc = analogDigitalRead(PULSE_PIN_A, READ_PIN_A);
+  uint8_t adc2 = analogDigitalRead(PULSE_PIN_B, READ_PIN_B);
+
+  while (adc != 1 || adc2 != 1){
+    adc = analogDigitalRead(PULSE_PIN_A, READ_PIN_A);
+    adc2 = analogDigitalRead(PULSE_PIN_B, READ_PIN_B);
+    Serial.print(adc);
+    Serial.print(" ");
+    Serial.print(adc2);
+    Serial.println("");
+
+    delay(500);
+
+    for(int i=0;i<2;i++){
+      tone(SPEAKER, freq, t);
+      delay(200);
+      noTone(SPEAKER);
+    }
   }
+  
+  wifi_init();
+  esp_now_start();
   esp_now_register_send_cb(OnDataSent);
+
 }//end setup
 
 void loop() {
@@ -137,28 +178,27 @@ void loop() {
   uint8_t adc = analogDigitalRead(PULSE_PIN_A, READ_PIN_A);
   uint8_t adc2 = analogDigitalRead(PULSE_PIN_B, READ_PIN_B);
 
-  // Serial.print(adc);
-  // Serial.print(" ");
-  // Serial.print(adc2);
-  // Serial.println("");
+  #ifdef DEBUG_INPUT
+    Serial.print(adc);
+    Serial.print(" ");
+    Serial.print(adc2);
+    Serial.println("");
+  #endif
+
   delay(1);
 
   // Construir o pacote de dados
   //strcpy(myData.data, "Hello world");
 
-  //myData.servoA < 180 ? myData.servoA += 10 : myData.servoA = 0;
-  //myData.servoB < 180 ? myData.servoB += 10 : myData.servoB = 0;
-
   myData.servoA = adc;
   myData.servoB = adc2;
-
   myData.led = !myData.led;
-
-  //Serial.println(myData.servoA);
-  //Serial.println(myData.servoB);
 
   // Enviar o pacote de dados para o receptor
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-  //Serial.println(result == ESP_OK?"PKG_OK":"PKG_ERROR");
+  
+  #ifdef DEBUG_PKG
+    Serial.println(result == ESP_OK?"PKG_OK":"PKG_ERROR");
+  #endif
   //delay(1000);
 }//end loop
